48b659b172023e8fb4007defae57cfee8bb2f245
Luca Saccarola <github.e41mv@aleeas.com>
2026-03-29 21:05:03 +0200

    feat: add gx command

--- vintage_commands.py
+++ vintage_commands.py
@@ -1,10 +1,24 @@
+import os
 import sublime
 import sublime_plugin
-import os
+import sys
+
+
+OPEN_CMD = ""
+if sys.platform == "linux":
+    OPEN_CMD = "xdg-open"
+elif sys.platform == "darwin":
+    OPEN_CMD = "open"
+elif sys.platform == "win32":
+    OPEN_CMD = 'start "" /b'
+elif sys.platform == "cygwin":
+    OPEN_CMD = 'cygstart --hide'
+
 
 def is_legal_path_char(c):
     # XXX make this platform-specific?
-    return c not in " \n\"|*<>{}[]()"
+    return c not in ' \n"|*<>{}[]()'
+
 
 def move_while_path_character(view, start, is_at_boundary, increment=1):
     while True:
@@ -14,6 +28,26 @@ def move_while_path_character(view, start, is_at_boundary, increment=1):
         if is_at_boundary(start):
             break
     return start
+
+
+class ViOpenLinkUnderSelectionCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        sel = self.view.sel()[0]
+        if not sel.empty():
+            url = self.view.substr(sel)
+        else:
+            caret_pos = self.view.sel()[0].begin()
+            current_line = self.view.line(caret_pos)
+
+            left = move_while_path_character(
+                self.view, caret_pos, lambda x: x < current_line.begin(), increment=-1
+            )
+            right = move_while_path_character(
+                self.view, caret_pos, lambda x: x > current_line.end(), increment=1
+            )
+            url = self.view.substr(sublime.Region(left + 1, right))
+        os.system(f"{OPEN_CMD} {url}")
+
 
 class ViOpenFileUnderSelectionCommand(sublime_plugin.TextCommand):
     def run(self, edit):
@@ -25,25 +59,21 @@ class ViOpenFileUnderSelectionCommand(sublime_plugin.TextCommand):
             current_line = self.view.line(caret_pos)
 
             left = move_while_path_character(
-                                            self.view,
-                                            caret_pos,
-                                            lambda x: x < current_line.begin(),
-                                            increment=-1)
+                self.view, caret_pos, lambda x: x < current_line.begin(), increment=-1
+            )
             right = move_while_path_character(
-                                            self.view,
-                                            caret_pos,
-                                            lambda x: x > current_line.end(),
-                                            increment=1)
+                self.view, caret_pos, lambda x: x > current_line.end(), increment=1
+            )
             file_name = self.view.substr(sublime.Region(left + 1, right))
 
-        file_name = os.path.join(os.path.dirname(self.view.file_name()),
-                                    file_name)
+        file_name = os.path.join(os.path.dirname(self.view.file_name()), file_name)
         if os.path.exists(file_name):
             self.view.window().open_file(file_name)
-            
+
+
 class ViSaveAndExit(sublime_plugin.WindowCommand):
     def run(self):
-        self.window.run_command('save')
-        self.window.run_command('close')
+        self.window.run_command("save")
+        self.window.run_command("close")
         if len(self.window.views()) == 0:
-            self.window.run_command('close')
+            self.window.run_command("close")
